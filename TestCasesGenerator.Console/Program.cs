b8602d183663217f3fb33ff0191d935136da67bd
Khaled Mohamed <khal3d.mohamed@gmail.com>
2019-05-14 16:02:29 +0200

    Initial Commit

---
+++ TestCasesGenerator.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using TestCasesGenerator.Core;
+
+namespace TestCasesGenerator.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var core = new CoreClass();
+            core.Test();
+        }
+    }
+}
