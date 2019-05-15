907a2cb3fe88c13cb53d5e1d90e321997dd35039
Khaled Mohamed <khal3d.mohamed@gmail.com>
2019-05-15 16:29:39 +0200

    Implemented File Reader

---
+++ TestCasesGenerator.Core/FileReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace TestCasesGenerator.Core
+{
+    public class FileReader
+    {
+        public String[] ReadFile(String filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                return File.ReadAllLines(filePath);
+            }
+            else
+            {
+                throw new Exception("File Not Found!");
+            }
+        }
+    }
+}
